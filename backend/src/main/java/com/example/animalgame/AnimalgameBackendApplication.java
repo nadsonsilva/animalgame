3b39a290e0fa8302a33a9ff755018a71fcd3e425
Nadson <nadsondasilvaesilva@gmail.com>
2026-02-24 16:39:16 -0400

    Adicionando a pasta backend com a estrutura do projeto e configurações atualizadas, incluindo Docker, configuração do banco de dados, e ajustes de dependências

---
+++ backend/src/main/java/com/example/animalgame/AnimalgameBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.animalgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalgameBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalgameBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/animalgame/AnimalgameBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.animalgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalgameBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
