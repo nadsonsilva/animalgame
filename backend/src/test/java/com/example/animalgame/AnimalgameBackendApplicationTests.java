8a3c870cfc76e985fa6f81ef66d648d0d4ba9043
Nadson <nadsondasilvaesilva@gmail.com>
2026-03-12 09:49:38 -0400

    corrigir testes do backend com perfil H2

--- backend/src/test/java/com/example/animalgame/AnimalgameBackendApplicationTests.java
+++ backend/src/test/java/com/example/animalgame/AnimalgameBackendApplicationTests.java
@@ -2,12 +2,14 @@ package com.example.animalgame;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AnimalgameBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
